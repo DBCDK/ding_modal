bc64ed47f9b83ea5d341aa03ecf18f12a7ddca43
pjohans <pjohans@gmail.com>
2016-08-10 10:19:53 +0200

    attach behaviours on context + leave out ding_modal_events

--- js/ding_modal.js
+++ js/ding_modal.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
 
   var DingModal = {};
 
-  DingModal.detachBehaviorsOnModal = function(reattach) {
+  DingModal.detachBehaviorsOnModal = function (reattach) {
     var reattach = reattach || false;
     var modal = document.getElementById('ding-modal');
     Drupal.detachBehaviors(modal, null, null);
@@ -11,57 +11,54 @@
     }
   };
 
-  DingModal.attachBehaviorsOnModal = function() {
+  DingModal.attachBehaviorsOnModal = function () {
     var modal = document.getElementById('ding-modal');
     Drupal.attachBehaviors(modal, null);
   };
 
-  DingModal.setLinkActions = function(context) {
+  DingModal.setLinkActions = function (context) {
     if (Drupal.settings.ding_modal) {
       // Rewrite links
-      var attachBehaviors = 0;
       for (var i in Drupal.settings.ding_modal.ding_modal_settings) {
-          if (Drupal.settings.ding_modal.ding_modal_settings.hasOwnProperty(i)) {
-              for (var n in Drupal.settings.ding_modal.ding_modal_settings[i].selectors) {
-                  var id = Drupal.settings.ding_modal.ding_modal_settings[i].id;
-                  var selector = Drupal.settings.ding_modal.ding_modal_settings[i].selectors[n];
-                  $(selector, context).once('ding-modal', function () { // adds a ding-modal-processed class
-                      $(selector).attr({
-                          'data-reveal-id': 'ding-modal',
-                          'data-reveal-ajax': 'true',
-                      }).addClass('use-ajax');
-                      attachBehaviors = 1;
-                  });
-              }
+        if (Drupal.settings.ding_modal.ding_modal_settings.hasOwnProperty(i)) {
+          for (var n in Drupal.settings.ding_modal.ding_modal_settings[i].selectors) {
+            var id = Drupal.settings.ding_modal.ding_modal_settings[i].id;
+            var selector = Drupal.settings.ding_modal.ding_modal_settings[i].selectors[n];
+            $(selector, context).once('ding-modal', function () { // adds a ding-modal-processed class
+              $(selector).attr({
+                'data-reveal-id': 'ding-modal',
+                'data-reveal-ajax': 'true'
+              }).addClass('use-ajax');
+              Drupal.attachBehaviors($(selector));
+            });
           }
-      }
-      if ( attachBehaviors == 1 ) {
-        Drupal.attachBehaviors();
-        attachBehaviors = 0;
+        }
       }
     } else {
       console.log('WARNING: Drupal.settings.ding_modal is undefined');
     }
   };
 
-  DingModal.addAccessibilityInfo = function(context) {
+  DingModal.addAccessibilityInfo = function (context) {
     var $modal = $("#ding-modal", context);
     $modal.attr("aria-hidden", false);
     $modal.attr("role", 'dialog');
   };
 
-  DingModal.removeAccessibilityInfo = function() {
+  DingModal.removeAccessibilityInfo = function () {
     var $modal = $("#ding-modal");
     $modal.attr("aria-hidden", true);
     $modal.removeAttr("role");
   };
 
   Drupal.behaviors.ding_modal = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
+      console.log('TESTHSET');
+      console.log(context);
       DingModal.setLinkActions(context);
     },
 
-    detach: function(context) {
+    detach: function (context) {
       DingModal.removeAccessibilityInfo(context);
     }
 

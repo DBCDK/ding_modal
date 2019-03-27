e5cd8fa7309ec80ad38a67cf1fdff00c28946360
Jørgen G. Nielsen <jgn@dbc.dk>
2019-03-27 11:30:23 +0100

    672: do not attach behaviours recursively

--- js/ding_modal.js
+++ js/ding_modal.js
@@ -30,7 +30,6 @@
                 'data-reveal-id': 'ding-modal',
                 'data-reveal-ajax': 'true'
               }).addClass('use-ajax');
-             DingModal.customAttach(selector, context);
             });
           }
         }
@@ -40,31 +39,7 @@
     }
   };
 
-  /**
-   * Wrapper for Drupal attach behaviors. Make sure it is only done once.
-   *
-   * Here is a hack to prevent
-   * Check if selector represents a list - probably a class selector - if so
-   * grab the parentnode for context and hope it is unique.
-   *
-   * NOTE this will not work if parentnode is also a selector defined in HOOK_ding_modal_menus
-   *
-   * @param selector
-   * @param context
-   */
-  DingModal.customAttach = function (selector, context) {
-     var element = $(selector);
-     if (element.length > 1) {
-       element.once('custom-attach').each(function (index) {
-         context = $(this)[0].parentNode;
-         Drupal.attachBehaviors(context);
-       })
-     }
-     else{
-       Drupal.attachBehaviors(context);
-     }
-   };
-   
+
   /**
    * Handling printing from modal window.
    */
